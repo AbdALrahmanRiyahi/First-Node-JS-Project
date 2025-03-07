b2940a83fe94fa10fd698ceb06b9aa6be1ccf381
AbdALrahmanRiyahi <abd1999abd27@gmail.com>
2025-03-07 15:27:20 +0300

    update

---
+++ app.js
@@ -0,0 +1,73 @@
+let mystudents = [
+    {
+      name: "omar",
+      age: 20,
+      major: "cs",
+    },
+    {
+      name: "anas",
+      age: 23,
+      major: "MIs",
+    },
+    {
+      name: "sara",
+      age: 22,
+      major: "IT",
+    },
+    {
+      name: "ali",
+      age: 24,
+      major: "SE",
+    },
+    {
+      name: "reem",
+      age: 21,
+      major: "CS",
+    },
+    {
+      name: "khaled",
+      age: 25,
+      major: "MIS",
+    },
+    {
+      name: "lina",
+      age: 23,
+      major: "Data Science",
+    },
+    {
+      name: "mohammed",
+      age: 26,
+      major: "AI",
+    },
+    {
+      name: "fatima",
+      age: 22,
+      major: "Cybersecurity",
+    },
+    {
+      name: "ahmad",
+      age: 24,
+      major: "Cloud Computing",
+    },
+    {
+      name: "nour",
+      age: 21,
+      major: "IT",
+    },
+    {
+      name: "tariq",
+      age: 25,
+      major: "Software Engineering",
+    }
+  ];
+  
+  console.log(mystudents);
+  
+
+for (let i = 0; i < mystudents.length; i++) {
+  console.log(
+    `the student name is ${mystudents[i].name} and his/her major is ${mystudents[i].major}`
+  );
+}
+
+console.log(mystudents);
