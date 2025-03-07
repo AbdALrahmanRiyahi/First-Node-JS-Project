770ae16a36de4c2926e54d6585c4b8641bea4146
AbdALrahmanRiyahi <abd1999abd27@gmail.com>
2025-03-07 16:21:23 +0300

    update

---
+++ ArrayMethods.js
@@ -0,0 +1,12 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log(fruits)
+
+console.log(fruits.length)      //1- length => returns the length
+console.log(fruits.at(2))       //2- at => Get the element
+fruits.pop();                   //3- pop => removes the last element from an array
+fruits.push("Kiwi");            //4- push => adds a new element to an array (at the end)
+fruits.shift();                 //5- shift => removes the first array element
+fruits.unshift("Lemon");        //6- unshift => adds a new element to an array (at the beginning)
+
+console.log(fruits)
